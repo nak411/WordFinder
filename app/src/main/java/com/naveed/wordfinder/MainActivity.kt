eee74d0ddcf2bf47e3751209a92e07c2d28d50b0
Naveed <nak411@gmail.com>
2020-09-22 16:24:05 -0400

    Clean up

--- app/src/main/java/com/naveed/wordfinder/MainActivity.kt
+++ app/src/main/java/com/naveed/wordfinder/MainActivity.kt
@@ -10,33 +10,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(p0: String?): Boolean {
-                return  true
+                return true
             }
-
             override fun onQueryTextChange(p0: String?): Boolean {
                 onTextChanged(p0)
                 return true
             }
-
         })
     }
 
     private fun onTextChanged(str: String?) {
-        Log.d("TAG",  "Search String: $str")
         val textToProcess = tv_result.text.toString()
-        if (!str.isNullOrEmpty()) {
-            val resultText = textToProcess.replace("$str", "<span style='background-color:yellow;'>$str</span>", false)
-            tv_result.text = HtmlCompat.fromHtml(resultText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        val toSearch = if (str.isNullOrEmpty()) "" else str
+        val highlighter = "<span style='background-color:yellow;'>$toSearch</span>"
+        val resultText = if (!str.isNullOrEmpty()) {
+            textToProcess.replace(toSearch, highlighter, false)
         } else {
-            val resultText = textToProcess.replace( "<span style='background-color:yellow;'></span>", "", false)
-            tv_result.text = HtmlCompat.fromHtml(resultText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            textToProcess.replace(highlighter, "", false)
         }
+        tv_result.text = HtmlCompat.fromHtml(resultText, HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
